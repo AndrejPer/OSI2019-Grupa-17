5ecd01f995ce1127a5e3e816dce8b83cc1739042
AndrejPer <43759884+AndrejPer@users.noreply.github.com>
2019-12-28 13:29:25 +0100

    Logging in as an admin

--- main.c
+++ main.c
@@ -13,6 +13,35 @@ int main()
         printf("Admin [A] or Guest [G]: \n");
         scanf(" %c", &account);
         //printf("%c", account);
+        
+        if(account == 'A')
+        {
+            int attempts = 5; p = 0;
+            //attempts is used to limit number of attempts
+            //p is used as a flag for (in)correct login data, 0 for incorrect
+            FILE facc = fopen("Accounts", r);
+            //this is the file where the login data is stored
+            char usern[10], pass[10];
+            //string for collecting username and password
+            
+            do
+            {
+                printf("Username: ");
+                scanf(" %s", usern);
+                printf("Password: ");
+                scanf(" %s", pass);
+                p = checking_login(usern, pass);
+                
+                if(!p) printf("\nIncorrect username of password.\n");
+                
+                else 
+                    printf("Loging in...");
+            }
+            while(!p && --attempts >=0)
+        }
+        
+        
+        
     }
     while(begin);
 
