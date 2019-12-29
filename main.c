d493782902570ad701ffd04423ab652e85b6d16c
AndrejPer <43759884+AndrejPer@users.noreply.github.com>
2019-12-29 15:45:08 +0100

    Loging in as admin
    I added the do while loop for admin login with max three attemps.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int checking_login(char*, char*, FILE*)
+
 int main()
 {
     char begin = 0, account = 0;
@@ -16,10 +18,10 @@ int main()
         
         if(account == 'A')
         {
-            int attempts = 5; p = 0;
+            int attempts = 3, p = 0;
             //attempts is used to limit number of attempts
             //p is used as a flag for (in)correct login data, 0 for incorrect
-            FILE facc = fopen("Accounts", r);
+            FILE *facc = fopen("Accounts.txt", "r");
             //this is the file where the login data is stored
             char usern[10], pass[10];
             //string for collecting username and password
@@ -30,14 +32,22 @@ int main()
                 scanf(" %s", usern);
                 printf("Password: ");
                 scanf(" %s", pass);
-                p = checking_login(usern, pass);
+                p = checking_login(usern, pass, facc);
                 
                 if(!p) printf("\nIncorrect username of password.\n");
                 
                 else 
-                    printf("Loging in...");
+                {   printf("Loging in...\n\n");
+                    printf("Welcome %s", username);
+                }
+                
             }
-            while(!p && --attempts >=0)
+            while(!p && --attempts >= 0);
+            
+            if(p)
+            {
+                //admin menu
+            }
         }
         
         
