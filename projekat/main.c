1225bd6e3eea561d0c75767127a959184d1f8847
andjeladragosavljevic <andjadragos@icloud.com>
2019-12-30 13:55:27 +0100

    Checking login
    A function which checks whether username and password are correct
    
    Delete event
    
    A function which which deletes a certain event
    
    View events
    
    A function which prints all events to the standard output

--- projekat/main.c
+++ projekat/main.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int checking_login(char*, char*, FILE*);
+int checking_login(char username[], char password[], FILE *fp)
+{
+    char temp_username[20],  temp_password[20];
 
+        while (fscanf(fp, "%s %s",temp_username, temp_password )!=EOF)
+        {
+
+            if(strcmp(username,temp_username)==0 && strcmp(password, temp_password)==0)
+                return 1;
+        }
+    return 0;
+
+}
+void delete_event()
+{
+    FILE *fp1, *fp2;
+    char name[20], temp[20], opis[20], lok[20], kategorija[20], datum[20], vrijeme[20];
+
+    if(fp1=fopen("Events.txt", "r"))
+    {
+        while(fscanf(fp1, "%s %s %s %s %s %s", name, opis, lok, kategorija, datum, vrijeme)!=EOF)
+            printf("%s %s %s %s %s %s\n", name, opis, lok, kategorija, datum, vrijeme);
+        rewind(fp1);
+            printf("Name the event you wish to delete ");
+            scanf("%s", temp);
+            while(fscanf(fp1, "%s %s %s %s %s %s", name, opis, lok, kategorija, datum, vrijeme)!=EOF)
+                if(strcmp(name,temp)==0)
+                {
+                    if(fp2=fopen("Replica.txt", "w"))
+                {
+                    rewind(fp1);
+                    while(fscanf(fp1, "%s %s %s %s %s %s", name, opis, lok, kategorija, datum, vrijeme)!=EOF)
+                        if(strcmp(name, temp)!=0)
+                        fprintf(fp2, "%s %s %s %s %s %s\n", name, opis, lok, kategorija, datum, vrijeme);
+
+                fclose(fp2);
+                }
+                }
+        fclose(fp1);
+        }
+        remove("Events.txt");
+        rename("Replica.txt", "Events.txt");
+        if(fp2=fopen("Events.txt", "r"))
+        while(fscanf(fp2, "%s %s %s %s %s %s", name, opis, lok, kategorija, datum, vrijeme)!=EOF)
+            printf("%s %s %s %s %s %s\n", name, opis, lok, kategorija, datum, vrijeme);
+
+}
+
+void view_events()
+{
+    FILE *fp;
+    char name[20], opis[20], lok[20], kategorija[20], datum[20], vrijeme[20];
+    if(fp=fopen("Events.txt", "r"))
+    while(fscanf(fp, "%s %s %s %s %s %s", name, opis, lok, kategorija, datum, vrijeme)!=EOF)
+    printf("%s %s %s %s %s %s\n", name, opis, lok, kategorija, datum, vrijeme);
+}
 int main()
 {
     char begin = 0, account = 0;
@@ -23,7 +78,7 @@ int main()
             //p is used as a flag for (in)correct login data, 0 for incorrect
             FILE *facc = fopen("Accounts.txt", "r");
             //this is the file where the login data is stored
-            char usern[10], pass[10];
+            char usern[30], pass[30];
             //string for collecting username and password
 
             do
@@ -38,7 +93,7 @@ int main()
 
                 else
                 {   printf("Loging in...\n\n");
-                    printf("Welcome %s", usern);
+                    printf("Welcome %s", usern);//username
                 }
 
             }
@@ -59,7 +114,11 @@ int main()
                 printf("\tAdd event category [T]\n");
                 printf("\tBack [B]\n");
 
-                scanf(" %c", &admin_choice);
+                scanf(" %c", &admin_choice);// ;
+                if(admin_choice=='D')
+                    delete_event();
+                    if(admin_choice=='V')
+                        view_events();
             }
         }
 
